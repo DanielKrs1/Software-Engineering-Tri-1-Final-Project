4c070709eeb9aa1bdc80aab7feaa10e167fcd0c2
seognaMcitpyrC@gmail.com <jerlee25@bergen.org>
2023-11-17 15:18:00 -0500

    Projectile Creating In Progress

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.MPE;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    public GameObject projectilePrefab;
     void Start()
     {
         
@@ -14,11 +17,24 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
 
+
         float horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * Time.deltaTime * horizontalInput);
 
         float verticalInput = Input.GetAxis("Vertical");
         transform.Translate(Vector3.up * Time.deltaTime * verticalInput);
 
+        if (Input.GetMouseButtonDown(0))
+
+        {
+            
+            Vector3 targetDir = Input.mousePosition - transform.position;
+            Quaternion projectileRotation = transform.rotation;
+            projectileRotation.z = Vector3.Angle(targetDir, Vector3.up);
+
+            Instantiate(projectilePrefab, transform.position, transform.rotation);
+        }
+
     }
+    
 }
